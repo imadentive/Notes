14b356b53b028bbb2447c8cb71ae8f74c0bed13d
Desmond Dai <desmonddai583@gmail.com>
2018-06-09 12:40:04 +0800

    Update basic.py

--- Python/basic.py
+++ Python/basic.py
@@ -34,6 +34,10 @@ aa = u'test'  # 轉爲unicode類型
 
 # sys.setrecursionlimit(1000)设定递归的最多次数
 
+# 关键字参数 e.g. def add(x, y),  add(y=3, x=4)
+# 可变参数 e.g. def demo(*param), demo(1,2,3,4) 或者 a = (1,2,3,4) demo(*a)
+# 关键字可变参数 e.g. def demo(**param), demo(a='123', b='566')
+
 # 將列表轉換位迭代器
 iter([1, 2, 3]) # 此時該對象就可以通過next方法調用
 
