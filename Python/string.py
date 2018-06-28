08b7cce309fc0a5acb4cd23531ea6583b962a934
desmonddai583 <desmonddai583@gmail.com>
2018-06-28 15:37:37 +0800

    add python string notes

--- Python/string.py
+++ Python/string.py
@@ -54,3 +54,48 @@ re.sub(r'(?P<d>\d{4})-(?P<m>\d{2})-(?P<y>\d{2})', r'\g<m>/\g<d>/\g<y>', log)
 # 正常使用for循環等方法在時間與空間上都有浪費
 # 推薦使用join方法
 ':'.join(['abc', '123', '456'])
+
+
+
+# 對字符串進行左右中對齊
+s = 'abc'
+# 1
+# 左對齊10爲不夠用第二個參數定義的字符串補(默認爲space),該補位字符串只可包含一個字符
+# 小於原本的長度則不做處理，直接使用原本字符串
+s.ljust(10, '^') 
+s.rjust(10, '^') 
+s.center(10, '^')
+
+# 2
+format(s, '<10') # 左
+format(s, '>10') # 右
+format(s, '^10') # 居中
+format(s, '*^10') # 居中並填充*
+# format也可以傳入數字等, 只要對象的類實現了__format__方法即可使用
+# 關於數字
+format(123, '+') # '123'
+format(-123, '+') # '-123'
+format(-123, '>+10') # 輸出符號並且右對齊
+format(-123, '=+10') # 輸出符號並且右對齊，但是符號會放在最左邊
+# 輸出符號並且右對齊，但是符號會放在最左邊，中間缺的爲用0補齊
+format(-123, '0=+10') 
+
+
+
+# 去掉字符串中不需要的字符
+s.strip() # 去掉兩端的space或者\t
+s.strip('=+-') # 去掉兩端=, +, -等字符
+# 或者使用string的replace方法以及re.sub方法
+# translate
+# 這裏字典的key一定要是ASCII碼
+s.translate({ord('a'): 'X', ord('b'): 'Y'})
+# maketrans 可以用來獲取映射字典
+s.translate(s.maketrans('abcxyz', 'XYZABC'))
+s.translate({ord('a'): None}) # 這裏利用translate去除字符串中的a
+
+# 例子, 去掉有拼音的字母
+s = 'nĭ hăo'
+import unicodedata
+# combining用來判斷是不是一個和combine字符, 如果是正常字符則返回0
+# dict.fromkeys會根據第一個參數的元素爲字典的key創建字典，第二個參數爲默認的value, 不指定默認爲None
+s.translate(dict.fromkeys([ord(c) for c in s if unicodedata.combining(c)]))
