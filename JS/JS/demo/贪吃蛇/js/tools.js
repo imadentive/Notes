7711c584176c27fa5eb8fdaaf0882e7afe5bbb88
desmonddai583 <desmonddai583@gmail.com>
2018-11-11 00:17:46 +0800

    add js notes

--- JS/JS/demo/贪吃蛇/js/tools.js
+++ JS/JS/demo/贪吃蛇/js/tools.js
@@ -1,5 +1,9 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
+(function () {
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    }
+
+    window.getRandomIntInclusive = getRandomIntInclusive;
+})()
